d1d2b641b6fb9635dfd34164cd5b9fad11b601e2
BryantChen23 <ms199323@gmail.com>
2023-08-16 13:52:56 +0800

    modified crawler and cancel dynamic operation

--- food_food_recommendation.py
+++ food_food_recommendation.py
@@ -4,35 +4,30 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import progressbar
 
-driver = webdriver.Edge()
-driver.get("https://www.ptt.cc/bbs/Food/index.html")  # 開啟範例網址
 
 titles = []
 authors = []
 dates = []
-i = 0
 
-while i <= 2:
-    i += 1
-    lp = driver.find_element(By.LINK_TEXT, "‹ 上頁")
-    time.sleep(3)
-    lp.click()
+with progressbar.ProgressBar(max_value=7002) as bar:
+    for i in range(7002, 5000, -1):
+        # 抓取每頁 標題、日期、作者
+        res = re.get("https://www.ptt.cc/bbs/Food/index%d.html" % i)
+        soup = BeautifulSoup(res.text, features="html.parser")
+        for item in soup.select(".r-ent"):
+            title = item.select(".title")[0].text.strip()
+            author = item.select(".author")[0].text.strip()
+            date = item.select(".date")[0].text.strip()
 
-    res = re.get("https://www.ptt.cc/bbs/Food/index.html")
-    soup = BeautifulSoup(res.text, features="html.parser")
-
-    for item in soup.select(".r-ent"):
-        title = item.select(".title")[0].text.strip()
-        author = item.select(".author")[0].text.strip()
-        date = item.select(".date")[0].text.strip()
-
-        if title[0:4] == "[食記]":
-            titles.append(title)
-            authors.append(author)
-            dates.append(date)
+            if title[0:4] == "[食記]":
+                titles.append(title)
+                authors.append(author)
+                dates.append(date)
+        time.sleep(0.1)
+        bar.update(i)
 
 res_df = pd.DataFrame({"發文日期": dates, "標題": titles, "作者": authors})
-print(res_df)
-
+print(len(res_df))
 res_df.to_csv("./foodfood_list.csv", index=False, encoding="utf-8-sig")
